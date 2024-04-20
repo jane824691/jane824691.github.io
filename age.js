163b3e0bf0f9bb2876b7e8017da5d043cb921391
Janet <jane824691@gmail.com>
2024-04-21 03:24:01 +0800

    微調css

--- age.js
+++ age.js
@@ -1,12 +1,18 @@
     //抓取HTML中的元素
     const userBirthday = document.getElementById('userBirthday');
     const AgeResult = document.getElementById('AgeResult')
-    // 事件聆聽, 當輸入input即執行動作
-    userBirthday = addEventListener("change", () => {
-        // 計算歲數, 用a傳值來取代userBirthday.value這一串, 讓算式看起來精簡
-        let AgeRes = translator(userBirthday.value)
-        AgeResult.innerText = AgeRes
-    })
+    // 事件聆聽, 使用者有輸入完才計算
+    if (userBirthday) {
+        userBirthday.addEventListener("change", () => {
+            if (userBirthday.value) { // 只有在 userBirthday 有值的情況下才執行
+                // 計算歲數, 用a傳值來取代userBirthday.value這一串, 讓算式看起來精簡
+                let AgeRes = translator(userBirthday.value);
+                AgeResult.innerText = AgeRes;
+            } else {
+                AgeResult.innerText = "請輸入生日";
+            }
+        });
+    }
 
     function translator(age) {
         // 公式：今天日期 - 出生日期 / 1年(毫秒) 1000 * 60 *60 *24 * 365.25 => 含每四年一閏月, 故補上0.25天
